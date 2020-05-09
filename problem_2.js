f4952ff829e85dc6f9cd19caede03e30d1f0e730
sdilbaz <serdarcan.dilbaz@gmail.com>
2020-05-09 10:38:58 +0300

    Problem 2 done

---
+++ problem_2.js
@@ -0,0 +1,351 @@
+db = new Mongo().getDB("project4");
+db.createCollection('famouspeople')
+fp=db.famouspeople;
+
+fp.insertMany([
+   {
+       "_id" : 1,
+       "name" : {
+           "first" : "John",
+           "last" : "Backus"
+       },
+       "birth" : ISODate("1924-12-03T05:00:00Z"),
+       "death" : ISODate("2007-03-17T04:00:00Z"),
+       "contribs" : [
+           "Fortran",
+           "ALGOL",
+           "Backus-Naur Form",
+           "FP"
+       ],
+       "awards" : [
+           {
+               "award" : "W.W. McDowell Award",
+               "year" : 1967,
+               "by" : "IEEE Computer Society"
+           },
+           {
+               "award" : "National Medal of Science",
+               "year" : 1975,
+               "by" : "National Science Foundation"
+           },
+           {
+               "award" : "Turing Award",
+               "year" : 1977,
+               "by" : "ACM"
+           },
+           {
+               "award" : "Draper Prize",
+               "year" : 1993,
+               "by" : "National Academy of Engineering"
+           }
+       ]
+   },
+   {
+       "_id" : ObjectId("51df07b094c6acd67e492f41"),
+       "name" : {
+           "first" : "John",
+           "last" : "McCarthy"
+       },
+       "birth" : ISODate("1927-09-04T04:00:00Z"),
+       "death" : ISODate("2011-12-24T05:00:00Z"),
+       "contribs" : [
+           "Lisp",
+           "Artificial Intelligence",
+           "ALGOL"
+       ],
+       "awards" : [
+           {
+               "award" : "Turing Award",
+               "year" : 1971,
+               "by" : "ACM"
+           },
+           {
+               "award" : "Kyoto Prize",
+               "year" : 1988,
+               "by" : "Inamori Foundation"
+           },
+           {
+               "award" : "National Medal of Science",
+               "year" : 1990,
+               "by" : "National Science Foundation"
+           }
+       ]
+   },
+   {
+       "_id" : 3,
+       "name" : {
+           "first" : "Grace",
+           "last" : "Hopper"
+       },
+       "title" : "Rear Admiral",
+       "birth" : ISODate("1906-12-09T05:00:00Z"),
+       "death" : ISODate("1992-01-01T05:00:00Z"),
+       "contribs" : [
+           "UNIVAC",
+           "compiler",
+           "FLOW-MATIC",
+           "COBOL"
+       ],
+       "awards" : [
+           {
+               "award" : "Computer Sciences Man of the Year",
+               "year" : 1969,
+               "by" : "Data Processing Management Association"
+           },
+           {
+               "award" : "Distinguished Fellow",
+               "year" : 1973,
+               "by" : " British Computer Society"
+           },
+           {
+               "award" : "W. W. McDowell Award",
+               "year" : 1976,
+               "by" : "IEEE Computer Society"
+           },
+           {
+               "award" : "National Medal of Technology",
+               "year" : 1991,
+               "by" : "United States"
+           }
+       ]
+   },
+   {
+       "_id" : 4,
+       "name" : {
+           "first" : "Kristen",
+           "last" : "Nygaard"
+       },
+       "birth" : ISODate("1926-08-27T04:00:00Z"),
+       "death" : ISODate("2002-08-10T04:00:00Z"),
+       "contribs" : [
+           "OOP",
+           "Simula"
+       ],
+       "awards" : [
+           {
+               "award" : "Rosing Prize",
+               "year" : 1999,
+               "by" : "Norwegian Data Association"
+           },
+           {
+               "award" : "Turing Award",
+               "year" : 2001,
+               "by" : "ACM"
+           },
+           {
+               "award" : "IEEE John von Neumann Medal",
+               "year" : 2001,
+               "by" : "IEEE"
+           }
+       ]
+   },
+   {
+       "_id" : 5,
+       "name" : {
+           "first" : "Ole-Johan",
+           "last" : "Dahl"
+       },
+       "birth" : ISODate("1931-10-12T04:00:00Z"),
+       "death" : ISODate("2002-06-29T04:00:00Z"),
+       "contribs" : [
+           "OOP",
+           "Simula"
+       ],
+       "awards" : [
+           {
+               "award" : "Rosing Prize",
+               "year" : 1999,
+               "by" : "Norwegian Data Association"
+           },
+           {
+               "award" : "Turing Award",
+               "year" : 2001,
+               "by" : "ACM"
+           },
+           {
+               "award" : "IEEE John von Neumann Medal",
+               "year" : 2001,
+               "by" : "IEEE"
+           }
+       ]
+   },
+   {
+       "_id" : 6,
+       "name" : {
+           "first" : "Guido",
+           "last" : "van Rossum"
+       },
+       "birth" : ISODate("1956-01-31T05:00:00Z"),
+       "contribs" : [
+           "Python"
+       ],
+       "awards" : [
+           {
+               "award" : "Award for the Advancement of Free Software",
+               "year" : 2001,
+               "by" : "Free Software Foundation"
+           },
+           {
+               "award" : "NLUUG Award",
+               "year" : 2003,
+               "by" : "NLUUG"
+           }
+       ]
+   },
+   {
+       "_id" : ObjectId("51e062189c6ae665454e301d"),
+       "name" : {
+           "first" : "Dennis",
+           "last" : "Ritchie"
+       },
+       "birth" : ISODate("1941-09-09T04:00:00Z"),
+       "death" : ISODate("2011-10-12T04:00:00Z"),
+       "contribs" : [
+           "UNIX",
+           "C"
+       ],
+       "awards" : [
+           {
+               "award" : "Turing Award",
+               "year" : 1983,
+               "by" : "ACM"
+           },
+           {
+               "award" : "National Medal of Technology",
+               "year" : 1998,
+               "by" : "United States"
+           },
+           {
+               "award" : "Japan Prize",
+               "year" : 2011,
+               "by" : "The Japan Prize Foundation"
+           }
+       ]
+   },
+   {
+       "_id" : 8,
+       "name" : {
+           "first" : "Yukihiro",
+           "aka" : "Matz",
+           "last" : "Matsumoto"
+       },
+       "birth" : ISODate("1965-04-14T04:00:00Z"),
+       "contribs" : [
+           "Ruby"
+       ],
+       "awards" : [
+           {
+               "award" : "Award for the Advancement of Free Software",
+               "year" : "2011",
+               "by" : "Free Software Foundation"
+           }
+       ]
+   },
+   {
+       "_id" : 9,
+       "name" : {
+           "first" : "James",
+           "last" : "Gosling"
+       },
+       "birth" : ISODate("1955-05-19T04:00:00Z"),
+       "contribs" : [
+           "Java"
+       ],
+       "awards" : [
+           {
+               "award" : "The Economist Innovation Award",
+               "year" : 2002,
+               "by" : "The Economist"
+           },
+           {
+               "award" : "Officer of the Order of Canada",
+               "year" : 2007,
+               "by" : "Canada"
+           }
+       ]
+   },
+   {
+       "_id" : 10,
+       "name" : {
+           "first" : "Martin",
+           "last" : "Odersky"
+       },
+       "contribs" : [
+           "Scala"
+       ]
+   }
+
+] )
+
+// 2.1
+var map1 = function(){
+    if ("awards" in this){
+        for (var idx = 0; idx < this["awards"].length; idx++) {
+            emit(this.awards[idx].award,1)
+        }
+    }
+}
+
+var red1 = function(key, value) {
+    return Array.sum(value);
+};
+
+fp.mapReduce(
+    map1,
+    red1,
+    { out: "q1" }
+)
+
+ // 2.2
+fp.aggregate(
+    [
+        {
+            $match:{"awards":{$exists:true}}
+        },
+        {
+            $project:{"awards":1}
+        },
+        {
+            $unwind: "$awards"
+        },
+        {
+            $group:{_id: "$awards.award",years: { $push: "$awards.year" }}
+        },
+        {
+            $out: "q2"
+        }
+    ]
+)
+
+// 2.3
+fp.aggregate(
+    [
+        {
+            $match:{"birth":{$exists:true}}
+        },
+        {
+            $project:{"year":{$year:"$birth"}}
+        },
+        {
+            $group:{ _id: "$year", ids: { $push: "$_id" }}
+        },
+        {
+            $addFields:{size:{$size:"$ids"}}
+        },
+        {
+            $out: "q3"
+        }
+    ]
+)
+
+// 2.4
+db.q4.insert(fp.find().sort({_id:1}).limit(1).toArray())
+db.q4.insert(fp.find().sort({_id:-1}).limit(1).toArray())
+
+// 2.5
+fp.createIndex( { "$**": "text" } )
+db.q5.insert(fp.find({ $text: { $search: "Turing" } } ).toArray())
+
+// 2.6
+fp.createIndex( { "$**": "text" } )
+db.q6.insert(fp.find({ $text: { $search: "Turing \"National Medal\"" } } ).toArray())
