307ae03ca7456d77f3a700e967a7b382c0c6b34e
sdilbaz <serdarcan.dilbaz@gmail.com>
2020-05-09 13:14:32 +0300

    Problem 3 completed

--- problem_3.js
+++ problem_3.js
@@ -1,18 +1,44 @@
 db = new Mongo().getDB("project4")
 
+db.categories.drop()
 db.categories.insertMany( [
-   { _id: "MongoDB", parent: "Databases" },
-   { _id: "dbm", parent: "Databases" },
-   { _id: "Databases", parent: "Programming" },
-   { _id: "Languages", parent: "Programming" },
-   { _id: "Programming", parent: "Books" },
-   { _id: "Books", parent: null }
-] )
+    { _id: "MongoDB", parent: "Databases" },
+    { _id: "dbm", parent: "Databases" },
+    { _id: "Databases", parent: "Programming" },
+    { _id: "Languages", parent: "Programming" },
+    { _id: "Programming", parent: "Books" },
+    { _id: "Books", parent: null }
+ ] )
 
 // 3.1
+var ancestors=[]
+var node="MongoDB"
+var level=1
+while (node!==null){
+    node=db.categories.findOne( { _id: node } ).parent
+    ancestors.push({Name:node,Level:level})
+    level++
+}
+ancestors.pop()
+print("3.1\tAncestors of MongoDB:",JSON.stringify(ancestors))
 
 // 3.2
+function find_ch(nodes){
+    var arrays=nodes.map(c => db.categories.find( { parent:  c} ).toArray())
+    var merged=[].concat.apply([], arrays);
+    return merged.map(a=>a._id)
+}
 
+db.categories.createIndex( { parent: 1 } )
+var depth=0
+nodes=["Books"]
+while (nodes.length!=0){
+    nodes=find_ch(nodes)
+    depth++
+}
+print("3.2\tDepth:",depth)
+
+db.categories.drop()
 db.categories.insertMany( [
     { _id: "MongoDB", children: [] },
     { _id: "dbm", children: [] },
